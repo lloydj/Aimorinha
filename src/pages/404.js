ba92ceac103c79920b234f38c87e074d9ff8cd78
SuLloyd <sulloyd@MacBook-Pro-de-Lloyd.local>
2018-08-27 14:41:55 -0300

    Webpack build bug fixed

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import React from 'react'
 const NotFoundPage = () => (
   <div>
     <h1>AI AI AI</h1>
-    <p>On est ou la? <link to = './'>Retour au normal</link></p>
+    <p>On est ou la? Retour au normal</p>
   </div>
 )
 
--- src/pages/VIP.js
+++ src/pages/VIP.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class About extends Component {
+export default class VIP extends Component {
   render() {
     return (
       <div>
